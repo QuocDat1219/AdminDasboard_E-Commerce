2eb75e771ced9bfad99df88f4d5ea823fa38565a
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-03-16 13:32:29 +0700

    Baocommit1603

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ import Addmenu from "./pages/Addmenu";
 import Menulist from "./pages/Menulist";
 import Addconfigchat from "./pages/Addconfigchat";
 import ConfigChatlist from "./pages/ConfigChatlist"
+import AddLink from "./pages/Addlink";
+import Linklist from "./pages/Linklist";
 function App() {
   return (
     <Router>
@@ -67,6 +69,8 @@ function App() {
           <Route path="menu-list" element={<Menulist />} />
           <Route path="config" element={<Addconfigchat />} />
           <Route path="config-list" element={<ConfigChatlist />}/>
+          <Route path="add-link" element={<AddLink/>}/>
+          <Route path="link-list" element={<Linklist/>}/>
         </Route>
       </Routes>
     </Router>
