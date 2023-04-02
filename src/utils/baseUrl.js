18f2a104dc1a20e99ace1b72e54180b75777b8ff
Lê Nguyễn Quốc Đạt <123617073+QuocDat1219@users.noreply.github.com>
2023-04-02 17:36:21 +0700

    new api

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
 export const base_url = "http://localhost:5000/api/";
-export const base_url_vercel = "https://backend-ecom3c.vercel.app/api/";
+export const base_url_vercel = "https://ecom-oto.vercel.app/api/";
