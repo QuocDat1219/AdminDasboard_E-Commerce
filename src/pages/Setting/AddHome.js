fcce3033e09029f35cb4407e51a35c3e589c1ae0
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-04-12 13:09:16 +0700

    commit

--- src/pages/Setting/AddHome.js
+++ src/pages/Setting/AddHome.js
@@ -12,6 +12,11 @@ const AddHome = () => {
   const [titleservice, setitleService] = useState("");
   const [descriptionservices, setDescriptionService] = useState("");
 
+  const [titlesanpham1, setTitlesanpham1] = useState("");
+  const [titlesanpham2, setTitlesanpham2] = useState("");
+  const [descriptionsanpham1, setDescriptionSanPham1] = useState("");
+  const [descriptionsanpham2, setDescriptionSanPham2] = useState("");
+
   useEffect(() => {
     const getdata = async () => {
       await axios
@@ -26,6 +31,10 @@ const AddHome = () => {
           setTitleProduct2(dataHome.titleprodcut2);
           setitleService(dataHome.titleservice);
           setDescriptionService(dataHome.descriptionservice);
+          setTitlesanpham1(dataHome.titlesanpham1);
+          setTitlesanpham2(dataHome.titlesanpham2);
+          setDescriptionSanPham1(dataHome.descriptionsanpham1);
+          setDescriptionSanPham2(dataHome.descriptionsanpham2);
         })
         .catch((error) => {
           console.log(error);
@@ -45,7 +54,10 @@ const AddHome = () => {
     formData.append("titleprodcut2", titleprodcut2);
     formData.append("titleservice", titleservice);
     formData.append("descriptionservice", descriptionservices);
-
+    formData.append("titlesanpham1", titlesanpham1);
+    formData.append("titlesanpham2", titlesanpham2);
+    formData.append("descriptionsanpham1", descriptionsanpham1);
+    formData.append("descriptionsanpham2", descriptionsanpham2);
     axios
       .put("https://ecom-oto.vercel.app/api/home/", formData, {
         headers: {
@@ -127,6 +139,66 @@ const AddHome = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 mt-2 focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:border-gray-600 dark:placeholder-black dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[98%] h-[200px]"
                 onChange={(e) => {
                   setTitleProduct2(e.target.value);
+                }}
+              />
+               <label
+                className="block mb-2 font-medium text-blue-500 "
+                for="file_input"
+              >
+                Title sản phẩm 1 (Sạc AC)
+              </label>
+              <input
+                type="text"
+                id="default-input"
+                value={titlesanpham1}
+                className="bg-gray-50 border border-gray-300 text-gray-900   focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:border-gray-600 dark:placeholder-black dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[98%]"
+                onChange={(e) => {
+                  setTitlesanpham1(e.target.value);
+                }}
+              />
+               <label
+                className="block mb-2 font-medium text-blue-500 "
+                for="file_input"
+              >
+                Mô tả sản phẩm 1 (Sạc AC)
+              </label>
+              <input
+                type="text"
+                id="default-input"
+                value={descriptionsanpham1}
+                className="bg-gray-50 border border-gray-300 text-gray-900   focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:border-gray-600 dark:placeholder-black dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[98%]"
+                onChange={(e) => {
+                  setDescriptionSanPham1(e.target.value);
+                }}
+              />
+               <label
+                className="block mb-2 font-medium text-blue-500 "
+                for="file_input"
+              >
+                Title sản phẩm 2(Sạc DC)
+              </label>
+              <input
+                type="text"
+                id="default-input"
+                value={titlesanpham2}
+                className="bg-gray-50 border border-gray-300 text-gray-900   focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:border-gray-600 dark:placeholder-black dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[98%]"
+                onChange={(e) => {
+                  setTitlesanpham2(e.target.value);
+                }}
+              />
+               <label
+                className="block mb-2 font-medium text-blue-500 "
+                for="file_input"
+              >
+                Mô tả sản phẩm 2 (Sạc DC)
+              </label>
+              <input
+                type="text"
+                id="default-input"
+                value={descriptionsanpham2}
+                className="bg-gray-50 border border-gray-300 text-gray-900   focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:border-gray-600 dark:placeholder-black dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[98%]"
+                onChange={(e) => {
+                  setDescriptionSanPham2(e.target.value);
                 }}
               />
             </div>
